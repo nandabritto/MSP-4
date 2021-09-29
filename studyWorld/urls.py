f99ded270856c00d2aaeb67aafa473d1267d2239
Sphere42 <sphere42@gmail.com>
2021-09-29 19:18:14 +0000

    Add static folder structure

--- studyWorld/urls.py
+++ studyWorld/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from django.config.urls.static import static
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
