3f6809bcc47b664b6efb3c57be3ab354d4d28307
Sphere42 <sphere42@gmail.com>
2021-09-29 19:49:57 +0000

    Add navbar.html into templates

--- studyWorld/settings.py
+++ studyWorld/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
