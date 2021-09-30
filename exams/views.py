cdd70203406c5c7441f886e9bbfbb1b476372ebc
Sphere42 <sphere42@gmail.com>
2021-09-30 17:47:47 +0000

    Add views for exam app

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Exam
+from django.views.generic import ListView
 
-# Create your views here.
+class ExamListView(ListView):
+    model = Exam
+    template_name = 'exam-list.html'
+
+def exam_view(request, pk):
+    exam = Exam.objects.get(pk=pk)
+    return render(request, 'exam-display.html', {'obj': exam})
+
