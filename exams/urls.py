ed47b81f49e493ab952249e71b51754a5cffda96
Sphere42 <sphere42@gmail.com>
2021-09-30 17:54:47 +0000

    Add exam app urls

---
+++ exams/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import (
+    ExamListView,
+    exam_view
+)
+
+app_name = 'exams'
+
+# Exam app specific urls
+
+urlpatterns = [
+    path('examlist', ExamListView.as_view(), name='main-view'),
+    path('examlist<pk>/', exam_view, name='exam_view')
+]
