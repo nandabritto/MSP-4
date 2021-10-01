f6d62da113548dbee4e1bcd9e7e4a4832bec206b
Sphere42 <sphere42@gmail.com>
2021-10-01 16:46:45 +0000

    Adding Second exam to database

--- exams/static/js/display.js
+++ exams/static/js/display.js
@@ -1,6 +1,9 @@
 const url = window.location.href
 console.log(url)
 
+const examBox = document.getElementById('exam-box')
+let data
+
 //The following will get the data required to display the available exams
 
 $.ajax({
@@ -8,6 +11,13 @@ $.ajax({
     url: `${url}data`,
     success: function(response){
         console.log(response)
+        data = response.data
+        data.forEach(el => {
+            for (const [question, answers] of Object.entries(el)){
+                console.log(question)
+                console.log(answers)
+            }
+        })
     },
     error: function(error){
         console.log(error)
