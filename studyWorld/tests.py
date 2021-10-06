11152a242edebf3fee636e18b9fde4bfb335d697
Sphere42 <sphere42@gmail.com>
2021-10-06 21:47:57 +0000

    Add unit tests for media and ide page display

--- studyWorld/tests.py
+++ studyWorld/tests.py
@@ -16,3 +16,33 @@ class HomePageTests(SimpleTestCase):
         response = self.client.get(reverse('index'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+class ResourceMediaTests(SimpleTestCase):
+
+    def test_media_status(self):
+        response = self.client.get('/resources/media')
+        self.assertEquals(response.status_code, 200)
+
+    def test_media_by_name(self):
+        response = self.client.get(reverse('resources-media'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_media_template(self):
+        response = self.client.get(reverse('resources-media'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'resources/media.html')
+
+class ResourceIDETests(SimpleTestCase):
+
+    def test_ide_status(self):
+        response = self.client.get('/resources/ide')
+        self.assertEquals(response.status_code, 200)
+
+    def test_ide_by_name(self):
+        response = self.client.get(reverse('resources-ide'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_ide_template(self):
+        response = self.client.get(reverse('resources-ide'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'resources/ide.html')
