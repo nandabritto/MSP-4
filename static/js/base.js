eb7b4c2b5367a694dc07dfba4e7b1fd6d102e8e1
Sphere42 <sphere42@gmail.com>
2021-10-06 22:10:25 +0000

    Add media section into style.css

--- static/js/base.js
+++ static/js/base.js
@@ -1,16 +1,16 @@
 // Creates the collapse animation for the Resources Media page //
 
-var col = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("collapsible");
 var i;
 
-for (i=0; i < HTMLAllCollection.length; i++) {
+for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
+        if (content.style.maxHeight){
             content.style.maxHeight = null;
         } else {
-            content.stylemaxHeight = content.scrolHeight + "px";
+            content.style.maxHeight = content.scrollHeight + "px";
         }
     });
 }
