3be7174f9715dda45f4c08ee5a67532c020cc395
Sphere42 <sphere42@gmail.com>
2021-10-20 17:24:22 +0000

    Add model for providers app

--- providers/models.py
+++ providers/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# Model for the provider section
+class Provider(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    url = models.CharField(max_length=50)
+    image = models.ImageField(uploaded_to='providers/images/', null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.text)
