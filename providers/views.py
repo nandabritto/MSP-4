37b92c02acb1e918fb13eced2d66a001da27830a
Sphere42 <sphere42@gmail.com>
2021-10-20 18:00:57 +0000

    Add basic view for provider app

--- providers/views.py
+++ providers/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.http import JsonResponse
+from .models import Question, Answer, Result, Exam
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+
+def upload(request):
+    if request.method == 'POST' and request.FILES['upload']:
+        upload = request.FILES['upload']
+        fss = FileSystemStorage()
+        file = fss.save(upload.name, upload)
+        file_url = fss.url(file)
+        return render(request, 'main/upload.html', {'file_url': file_url})
+    return render(request, 'main/upload.html')
