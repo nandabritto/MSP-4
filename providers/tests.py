ae23c9c73121d96fa01065f2527055d1a0da096a
Sphere42 <sphere42@gmail.com>
2021-10-21 17:24:50 +0000

    Add Unit tests for provider list page

--- providers/tests.py
+++ providers/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
+from django.urls import reverse, resolve
+from .models import Providers
+from . import views
 
 # Create your tests here.
+# Tests the url for the exam list page
+class ProvidersListPageTests(TestCase):
+
+    def test_provider_list_page_status_code(self):
+        response = self.client.get('/providers/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_provider_list_page_url_by_name(self):
+        response = self.client.get(reverse('providers:providers-list'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_provider_list_page_uses_correct_template(self):
+        response = self.client.get(reverse('providers:providers-list'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'provider-list.html')
