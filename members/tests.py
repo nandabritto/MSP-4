dd558c80b9ee2ef532515a963b5d6c5bd569b37d
Sphere42 <sphere42@gmail.com>
2021-10-28 19:00:30 +0000

    Add sign in page load unit test

--- members/tests.py
+++ members/tests.py
@@ -17,3 +17,19 @@ class MemberRegistrationPageTests(TestCase):
         response = self.client.get(reverse('members:member-register'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'register.html')
+
+# Test for the signin page load
+class MemberSignupPageTests(TestCase):
+    
+    def test_signup_page_status_code(self):
+        response = self.client.get('member-signin')
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_url_by_name(self):
+        response = self.client.get(reverse('members:member-signin'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_signup_uses_correct_template(self):
+        response = self.client.get(reverse('members:member-signin'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'signin.html')
