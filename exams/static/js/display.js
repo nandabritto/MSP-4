f661ec08d9d61193e2c1feaa44580401162fcaaa
Sphere42 <sphere42@gmail.com>
2021-11-07 21:02:40 +0000

    Correcting missing ; from js files

--- exams/static/js/display.js
+++ exams/static/js/display.js
@@ -1,64 +1,65 @@
+/*jshint esversion: 6 */
 // setting the window url for use in the ajax queries
-const url = window.location.href
+const url = window.location.href;
 
 // Setting the const for the html elements to be updated
-const examBox = document.getElementById('exam-box')
-const scoreDisplay = document.getElementById('scoredisplay')
-const resultDisplay = document.getElementById('resultsdisplay')
-const timerBox = document.getElementById('timerdisplay')
-const endButton = document.getElementById('endbutton')
-const passgrade = document.getElementById('passgrade')
+const examBox = document.getElementById('exam-box');
+const scoreDisplay = document.getElementById('scoredisplay');
+const resultDisplay = document.getElementById('resultsdisplay');
+const timerBox = document.getElementById('timerdisplay');
+const endButton = document.getElementById('endbutton');
+const passgrade = document.getElementById('passgrade');
 let timer;
 
 // Setting up the timer which counts down based on each individual exam limit
 const activateTimer = (time) => {
     if (time.toString().length < 2) {
-        timerBox.innerHTML = `<b>0${time}:00</b>`
+        timerBox.innerHTML = `<b>0${time}:00</b>`;
     } else {
-        timerBox.innerHTML = `<b>${time}:00</b>`
+        timerBox.innerHTML = `<b>${time}:00</b>`;
     }
 
-    let minutes = time - 1
-    let seconds = 60
-    let displaySeconds
-    let displayMinutes
+    let minutes = time - 1;
+    let seconds = 60;
+    let displaySeconds;
+    let displayMinutes;
     timer = setInterval(()=>{
-        seconds --
+        seconds --;
         if (seconds < 0) {
-            seconds = 59
-            minutes --
+            seconds = 59;
+            minutes --;
         }
         if (minutes.toString().length < 2) {
-            displayMinutes = '0'+ minutes
+            displayMinutes = '0'+ minutes;
         } else {
-            displayMinutes = minutes
+            displayMinutes = minutes;
         }
         if(seconds.toString().length < 2) {
-            displaySeconds = '0' + seconds
+            displaySeconds = '0' + seconds;
         } else {
-            displaySeconds = seconds
+            displaySeconds = seconds;
         }
         if (minutes === 0 && seconds === 0) {
             setTimeout(()=>{
-                clearInterval(timer)
-                alert('Time over')
-                sendData()
-            }, 500)
+                clearInterval(timer);
+                alert('Time over');
+                sendData();
+            }, 500);
         }
         // Actual countdown display
         timerBox.innerHTML = `
             <div>
                 <b class="btn-danger btn-lg p-2 timer">${displayMinutes}:${displaySeconds}</b>
-            </div>`
-    }, 1000)
-}
+            </div>`;
+    }, 1000);
+};
 
 // Automatically obtains the exam questions/answers on page load
 $.ajax({
     type: 'GET',
     url: `${url}data`,
     success: function(response){
-        const data = response.data
+        const data = response.data;
         data.forEach(el => {
             for (const [question, answers] of Object.entries(el)){
                 // Populates the question being asked
@@ -69,7 +70,7 @@ $.ajax({
                             <h5><b>Question: ${question}</b></h5>
                         </div>
                     </div>
-                `
+                `;
                 // Populates the answers which are matched to the previously chosen question
                 answers.forEach(answer=>{
                     examBox.innerHTML += `
@@ -81,36 +82,35 @@ $.ajax({
                                 </label>
                             </div>
                         </div>
-                    `
-                })
+                    `;
+                });
             }
         });
         // Activates the countdown timer once the questions have loaded
-        activateTimer(response.time)
-        
+        activateTimer(response.time);
     },
     error: function(error){
-        console.log(error)
+        console.log(error);
     }
-})
+});
 
-const examForm = document.getElementById('exam-form')
-const csrf = document.getElementsByName('csrfmiddlewaretoken')
+const examForm = document.getElementById('exam-form');
+const csrf = document.getElementsByName('csrfmiddlewaretoken');
 
 // Gets the Users answers and holds them to be compared with the correct answers in the DB
 const sendData = () => {
-    const elements = [...document.getElementsByClassName('ans')]
-    const data = {}
-    data['csrfmiddlewaretoken'] = csrf[0].value
+    const elements = [...document.getElementsByClassName('ans')];
+    const data = {};
+    data['csrfmiddlewaretoken'] = csrf[0].value;
     elements.forEach(el=>{
         if (el.checked) {
-            data[el.name] = el.value
+            data[el.name] = el.value;
         } else {
             if (!data[el.name]) {
-                data[el.name] = null
+                data[el.name] = null;
             }
         }
-    })
+    });
     // Functions to provide the "grading" of the submitted answers
     $.ajax({
         type: 'POST',
@@ -127,12 +127,12 @@ const sendData = () => {
                         <button class="btn btn-success float-right" onClick="window.location.reload();">Try Again</button>
                     </div>
                 </div>
-                `
-            const results = response.results
+                `;
+            const results = response.results;
             // Once answers have been submitted the timer, questions and required pass rate will disappear
-            examForm.classList.add('not-visible')
-            timerBox.classList.add('not-visible')
-            passgrade.classList.add('not-visible')
+            examForm.classList.add('not-visible');
+            timerBox.classList.add('not-visible');
+            passgrade.classList.add('not-visible');
 
             // prints out the overall score results
             if (response.passed) {
@@ -145,7 +145,7 @@ const sendData = () => {
                                 <img src="../static/images/results/pass.png" alt="Pass icon">
                             </div>
                         </div>
-                    </div>`
+                    </div>`;
             }
             else {
                 scoreDisplay.innerHTML =  `
@@ -157,14 +157,14 @@ const sendData = () => {
                                 <img src="../static/images/results/fail.png" alt="Fail icon">
                             </div>
                         </div>
-                </div>`
+                </div>`;
             }            
             
             // scoreDisplay.innerHTML = `${response.passed ? 'Congratulations! ' : 'Oops..:( '}Your result is ${response.score.toFixed(2)}%`
 
             // The below will populate the results for each question
             results.forEach(res=>{
-                const resDiv = document.createElement("div")
+                const resDiv = document.createElement("div");
                 for (const [question, resp] of Object.entries(res)){
 
                     // if no repose received - Correct answer is not displayed
@@ -182,8 +182,8 @@ const sendData = () => {
                     }
                     // if response received
                     else {
-                        const answer = resp['answered']
-                        const correct = resp['correct_answer']
+                        const answer = resp['answered'];
+                        const correct = resp['correct_answer'];
                         // If the answer received was correct
                         if (answer == correct) {
                             resDiv.innerHTML += `
@@ -195,7 +195,7 @@ const sendData = () => {
                                         <p> is: <b>${answer}</b></p>
                                     </div>
                                 </div>
-                          </div>`
+                          </div>`;
                         // If the answer received was incorrect
                         } else {
                             resDiv.innerHTML += `
@@ -207,22 +207,22 @@ const sendData = () => {
                                         <p> The correct answer is : <b>${correct}</b></p>
                                     </div>
                                 </div>
-                            </div>`
+                            </div>`;
                         }
                     }
                 }
                 // Collates all question/answer results to single print
-                resultDisplay.append(resDiv)
-            })
+                resultDisplay.append(resDiv);
+            });
         },
         error: function(error){
-            console.log(error)
+            console.log(error);
         }
-    })
-}
+    });
+};
 
 // Triggers the results event based on mouse click of Submit button
 examForm.addEventListener('submit', e=>{
-    e.preventDefault()
-    sendData()
-})
+    e.preventDefault();
+    sendData();
+});
--- exams/static/js/examlist.js
+++ exams/static/js/examlist.js
@@ -1,21 +1,22 @@
+/*jshint esversion: 6 */
 // Obtaining the applicable html elements
-const modalTitle = document.getElementById('exampleModalLabel')
-const modalBtns = [...document.getElementsByClassName('modal-button')]
-const modalBody = document.getElementById('modal-body-confirm')
-const startBtn = document.getElementById('start-button')
+const modalTitle = document.getElementById('exampleModalLabel');
+const modalBtns = [...document.getElementsByClassName('modal-button')];
+const modalBody = document.getElementById('modal-body-confirm');
+const startBtn = document.getElementById('start-button');
 
-const url = window.location.href
+const url = window.location.href;
 
 // initiates the modal when an exxam is selected
 modalBtns.forEach(modalBtn=> modalBtn.addEventListener('click', ()=>{
-    const pk = modalBtn.getAttribute('data-pk')
-    const name = modalBtn.getAttribute('data-exam')
-    const numQuestions = modalBtn.getAttribute('data-questions')
-    const difficulty = modalBtn.getAttribute('data-difficulty')
-    const scoreToPass = modalBtn.getAttribute('data-pass')
-    const time = modalBtn.getAttribute('data-time')
+    const pk = modalBtn.getAttribute('data-pk');
+    const name = modalBtn.getAttribute('data-exam');
+    const numQuestions = modalBtn.getAttribute('data-questions');
+    const difficulty = modalBtn.getAttribute('data-difficulty');
+    const scoreToPass = modalBtn.getAttribute('data-pass');
+    const time = modalBtn.getAttribute('data-time');
 
-    modalTitle.innerHTML = `<b>${name}</b>`
+    modalTitle.innerHTML = `<b>${name}</b>`;
 
     modalBody.innerHTML = `
         <div class="h5 mb-3">Are you sure you want to begin?</div>
@@ -27,9 +28,9 @@ modalBtns.forEach(modalBtn=> modalBtn.addEventListener('click', ()=>{
                 <li>Time: <b>${time} min</b></li>
             </ul>
         </div>
-    `
+    `;
 
     startBtn.addEventListener('click', ()=>{
-        window.location.href = url + pk
-    })
-}))
+        window.location.href = url + pk;
+    });
+}));
