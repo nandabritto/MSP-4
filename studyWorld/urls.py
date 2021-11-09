a89ac2730261dc110cff130e76906a1a37af1b13
Sphere42 <sphere42@gmail.com>
2021-11-09 18:02:30 +0000

    Refactor studyWorld urls inline with pep8

--- studyWorld/urls.py
+++ studyWorld/urls.py
@@ -7,13 +7,41 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('resources/', views.resourcesList, name='resources-list'),
-    path('resources/media', views.resourcesMedia, name='resources-media'),
-    path('resources/ide', views.resourcesIDE, name='resources-ide'),
-    path('resources/frameworks', views.resourcesFrameworks, name='resources-frameworks'),
-    path('resources/badges', views.resourcesBadges, name='resources-badges'),
-    path('resources/learning', views.resourcesLearning, name='resources-learning'),
-    path('resources/tools', views.resourcesTools, name='resources-tools'),
+    path(
+        'resources/',
+        views.resourcesList,
+        name='resources-list'
+        ),
+    path(
+        'resources/media',
+        views.resourcesMedia,
+        name='resources-media'
+        ),
+    path(
+        'resources/ide',
+        views.resourcesIDE,
+        name='resources-ide'
+        ),
+    path(
+        'resources/frameworks',
+        views.resourcesFrameworks,
+        name='resources-frameworks'
+        ),
+    path(
+        'resources/badges',
+        views.resourcesBadges,
+        name='resources-badges'
+        ),
+    path(
+        'resources/learning',
+        views.resourcesLearning,
+        name='resources-learning'
+        ),
+    path(
+        'resources/tools',
+        views.resourcesTools,
+        name='resources-tools'
+        ),
     path('', include('exams.urls', namespace='exams')),
     path('', include('providers.urls', namespace='providers')),
     path('', include('members.urls', namespace='members')),
